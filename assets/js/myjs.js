1818ad1c3969cce75280fe66037b5a4054a955a6
Adhiz <87264283+amarzyo@users.noreply.github.com>
2022-09-02 14:58:21 +0700

    Wedding Aas - Iqbal

---
+++ assets/js/myjs.js
@@ -0,0 +1,26 @@
+AOS.init({
+  once: false,
+  duration: 1000,
+});
+
+// Tampil modal di awal
+$(document).ready(function () {
+  $("#myModal").modal("show");
+});
+
+// Tombol Music
+var audio = document.getElementById("audio");
+var playPauseBTN = document.getElementById("playPauseBTN");
+var count = 0;
+
+function playPause() {
+  if (count == 0) {
+    count = 1;
+    audio.play();
+    playPauseBTN.innerHTML = '<i class="uil uil-music-tune-slash"></i> Pause';
+  } else {
+    count = 0;
+    audio.pause();
+    playPauseBTN.innerHTML = '<i class="uil uil-music"> Play';
+  }
+}
